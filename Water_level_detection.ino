1d2091cf6845341bb448f313e346279a8b6e9281
Neel Patel <neel_patel2004@outlook.com>
2025-03-02 17:42:25 -0500

    Create Water_level_detection.ino

---
+++ Water_level_detection.ino
@@ -0,0 +1,33 @@
+int adc_id = A0;
+int HistoryValue = 0;
+char printBuffer[128];
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+long  temp_max,temp_min, temp_value,temp;
+long value = 0;
+  temp_max  = analogRead(adc_id); // get adc value
+  temp_min  = temp_max;
+  temp_value= temp_max;
+    
+    for(int i = 0; i < 201; i ++ ){
+      temp =  analogRead(adc_id);
+      if(temp_max<temp)  temp_max = temp;
+       if(temp_min>temp) temp_min = temp;
+       temp_value += temp;
+      }
+      value =(temp_value-temp_max-temp_min)/200;
+    if(HistoryValue!=value)
+    {
+      sprintf(printBuffer,"ADC%d level is %d\n",adc_id, value);
+      Serial.print(printBuffer);
+      HistoryValue = value;  
+        delay(500);
+      }
+  
+}
